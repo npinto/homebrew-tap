50ec3e674de7f94affa0f71a1d4baeaa576b7c78
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-19 12:08:19 +0100

    BUG: append Np to cairo

--- cairo-np.rb
+++ cairo-np.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Cairo < Formula
+class CairoNp < Formula
   homepage 'http://cairographics.org/'
   url 'http://www.cairographics.org/releases/cairo-1.10.2.tar.gz'
   sha1 'ccce5ae03f99c505db97c286a0c9a90a926d3c6e'
@@ -14,6 +14,8 @@ class Cairo < Formula
   fails_with_llvm "Throws an 'lto could not merge' error during build.", :build => 2336
 
   def install
+    ENV.append "LDFLAGS",  `pkg-config --libs-only-L pixman-1`.chomp
+    ENV.append "CPPFLAGS",  `pkg-config --cflags-only-I pixman-1`.chomp
     ENV.x11
     args = %W[
       --disable-dependency-tracking
